bd54b5593ece5fcfe92b06bf2450969d1971e3e0
ANDRINIRINA Erick <ckandrinirina@gmail.com>
2019-07-25 23:36:00 +0300

    oko

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\RechercheType;
+use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends AbstractController
 {
     /**
      * @Route("/search", name="search")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+        $form = $this->createForm(RechercheType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted())
+        {
+            dump($form->getData());
+        }
+        return $this->render('search/index.html.twig',[
+            'form'=>$form->createView()
         ]);
     }
 }
